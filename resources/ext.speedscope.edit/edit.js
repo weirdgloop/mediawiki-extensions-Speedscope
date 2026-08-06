0c53c7546c5727d84637f103748910b8020d8307
SomeRandomDeveloper <git@srd.dev>
2026-08-06 23:55:43 +0200

    Fix live preview breaking if parser profiler is disabled

--- resources/ext.speedscope.edit/edit.js
+++ resources/ext.speedscope.edit/edit.js
@@ -3,14 +3,13 @@ $( () => {
 	const STORAGE_KEY = 'speedscope-profile-type';
 
 	const $selector = $( '#mw-speedscope-profile-type-selector' );
-	if ( !$selector.length ) {
-		return;
+	let dropdown = null;
+	if ( $selector.length ) {
+		dropdown = OO.ui.DropdownInputWidget.static.infuse( $selector );
+		dropdown.setValue( mw.storage.get( STORAGE_KEY ) || 'parser' );
+
+		dropdown.on( 'change', ( val ) => mw.storage.set( STORAGE_KEY, val ) );
 	}
-
-	const dropdown = OO.ui.DropdownInputWidget.static.infuse( $selector );
-	dropdown.setValue( mw.storage.get( STORAGE_KEY ) || 'parser' );
-
-	dropdown.on( 'change', ( val ) => mw.storage.set( STORAGE_KEY, val ) );
 
 	if ( mw.config.get( 'speedscopeUseLivePreview' ) ) {
 		$( '#wpPreview, #wpDiff' ).on( 'click', () => {
@@ -19,15 +18,19 @@ $( () => {
 		$( '#wpProfilePreview' ).on( 'click', ( event ) => {
 			$( '.mw-speedscope-profile-notice' ).remove();
 
+			const parseParams = {
+				wpProfilePreview: 1
+			};
+			if ( dropdown ) {
+				parseParams.wpProfileType = dropdown.getValue();
+			}
+
 			const promise = require( 'mediawiki.page.preview' ).doPreview( {
 				isLivePreview: true,
 				previewHeader: mw.message( 'preview' ).escaped(),
 				previewNote: mw.message( 'previewnote' ).parseDom(),
 				createSpinner: true,
-				parseParams: {
-					wpProfilePreview: 1,
-					wpProfileType: dropdown.getValue()
-				},
+				parseParams: parseParams,
 				responseHandler: ( config, response ) => {
 					const profileUrl = response.parse.jsconfigvars.speedscopeProfileUrl;
 					if ( !profileUrl ) {
