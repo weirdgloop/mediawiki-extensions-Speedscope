20e9f8d449dbd804643bdeaef67db860b91121a0
SomeRandomDeveloper <git@srd.dev>
2026-08-24 21:36:50 +0200

    Revert "oops"
    This reverts commit ca2681261eb2dc9cf709e555aff749f7c5721c41.

--- src/Profiler/ExcimerSpeedscopeProfiler.php
+++ src/Profiler/ExcimerSpeedscopeProfiler.php
@@ -117,7 +117,7 @@ class ExcimerSpeedscopeProfiler implements ISpeedscopeProfiler {
 			}
 		}
 
-		if ( true || !$this->profile->isForced() ) {
+		if ( !$this->profile->isForced() ) {
 			$statsdLogger = MediaWikiServices::getInstance()->getService( 'Speedscope.StatsdLogger' );
 			/** @var SpeedscopeStatsdLogger $statsdLogger */
 			$statsdLogger->processExcimerLog( $this->excimer->getLog() );
